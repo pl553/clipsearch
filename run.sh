1212d150684ae661916be501028fdf6b7e5e5115
pl553 <110288373+pl553@users.noreply.github.com>
2023-07-31 00:58:08 +0300

    image adding

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 
 trap 'trap " " SIGTERM; kill 0; wait;' SIGINT SIGTERM
 
-PORT=9081 node frontend/build &
-PORT=9080 ./backend/clipsearch
+PORT=9081 ORIGIN=http://localhost node frontend/build &
+PORT=9080 POSTGRESQL_URL=postgres://clipsearch:weakpassword@localhost:5432/clipsearch?sslmode=disable ./backend/clipsearch
 
 
 
