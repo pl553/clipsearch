fa3a1fc956460dfe4b2d10921ae1eaeb1cb9ca2c
pl553 <110288373+pl553@users.noreply.github.com>
2023-08-13 23:46:53 +0300

    Image searching

--- run.sh
+++ run.sh
@@ -3,6 +3,8 @@
 trap 'trap " " SIGTERM; kill 0; wait;' SIGINT SIGTERM
 
 PORT=9081 ORIGIN=http://localhost node frontend/build &
+(cd backend/clip_daemons && python3.11 search_daemon.py) &
+(cd backend/clip_daemons && python3.11 feature_extraction_daemon.py) &
 PORT=9080 POSTGRESQL_URL=postgres://clipsearch:weakpassword@localhost:5432/clipsearch?sslmode=disable ./backend/clipsearch
 
 
